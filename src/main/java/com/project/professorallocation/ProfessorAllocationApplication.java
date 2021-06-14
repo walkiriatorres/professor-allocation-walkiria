7f9c189ee2eb03be852f4f245af806bb23c7c9e3
Walkiria <walkiria_tg@hotmail.com>
2021-06-14 18:16:09 -0300

    adicionando projeto

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationApplication.class, args);
+	}
+
+}
